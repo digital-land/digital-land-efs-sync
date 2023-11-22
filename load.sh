71a0ec47dd7ad743b1616d881ff912970f19df09
ernest-tpximpact <121093104+ernest-tpximpact@users.noreply.github.com>
2023-11-22 14:13:00 +0000

    Refractor source into python (#1)
    * Refractor collection sync into python
    
    * Add entry point template
    
    * Update entry script
    
    * Add execute permission
    
    * Update-python file name
    
    * Update python logger property
    
    * Update logger
    
    * Update logger
    
    * Update logger
    
    * fix s3 attribute issue
    
    * Add more logging
    
    * Update getSpecifications method
    
    * Update getSpecifications method
    
    * Update constructor
    
    * Output entire specifications list
    
    * Update logger to remove formatter
    
    * Update logger level
    
    * Set logger level
    
    * Update shouldSync method
    
    * Update shouldSync method
    
    * Update logging
    
    * Update logging format
    
    * Update moveDatabase method
    
    * Update logging
    
    * Run black linting

---
+++ load.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+DATABASE=${S3_KEY##*/}
+export DATABASE_NAME=${DATABASE%.*}
+echo "DATABASE NAME: $DATABASE_NAME"
+echo "$EVENT_ID: running with settings: S3_BUCKET=$S3_BUCKET, S3_KEY=$S3_KEY, DATABASE=$DATABASE, DATABASE_NAME=$DATABASE_NAME"
+
+
+# Testing
+echo "$EVENT_ID: Run EFS Collection Sync"
+python3 -c "
+from collection_sync import CollectionSync
+
+# Create an instance of the CollectionSync
+collection_sync = CollectionSync('$EVENT_ID')
+
+# Call the processObject
+collection_sync.processObject('$S3_KEY', '$S3_BUCKET')
+"
